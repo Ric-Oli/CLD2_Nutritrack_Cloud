69c39335c597c68d4e1f29c96afee93d6c3c61b8
Ric-Oli <ricardooliveiraprivate@hotmail.com>
2026-02-12 12:02:19 +0100

    Forgot to commit before. Oops..

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,18 +3,75 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="parent-section" class="w-[95%] md:w-[85%] lg:w-[75%] mx-auto max-w-6xl">
+    <div class="text-center p-4">
+      <h1>NutriTrack Cloud</h1>
+    </div>
+
+
+    <!-- MAIN CONTENT -->
+    <div class="grid xl:grid-cols-2 p-8">
+      <div class="grid grid-cols-2 xl:mx-8">
+
+        <!-- CALORIES SECTION -->
+        <div id="calories-section" class="track-nutrition text-lg text-center font-semibold text-gray-200 tracking-wide uppercase m-3 p-3">
+          <h2 class="text-red-400">Calories</h2>
+          <div class="grid grid-cols-1 place-items-center mt-4">
+            <div class="w-32 h-32 rounded-full border-8 border-gray-200"></div>
+          </div>
+        </div>
+
+        <!-- PROTEINS SECTION -->
+        <div id="proteins-section" class="track-nutrition text-lg text-center font-semibold text-gray-200 tracking-wide uppercase m-3 p-3">
+          <h2 class="text-orange-400">Proteins</h2>
+          <div class="grid grid-cols-1 place-items-center mt-4">
+            <div class="w-32 h-32 rounded-full border-8 border-gray-200"></div>
+          </div>
+        </div>
+
+        <!-- CARBOHYDRATE SECTION -->
+        <div id="glucides-section" class="track-nutrition text-lg text-center font-semibold text-gray-200 tracking-wide uppercase m-3 p-3">
+          <h2 class="text-green-400">Carbohydrates</h2>
+          <div class="grid grid-cols-1 place-items-center mt-4">
+            <div class="w-32 h-32 rounded-full border-8 border-gray-200"></div>
+          </div>
+        </div>
+
+        <!-- LIPIDS SECTIONS -->
+        <div id="lipides-section" class="track-nutrition text-lg text-center font-semibold text-gray-200 tracking-wide uppercase m-3 p-3">
+          <h2 class="text-blue-400">Lipids</h2>
+          <div class="grid grid-cols-1 place-items-center mt-4">
+            <div class="w-32 h-32 rounded-full border-8 border-gray-200"></div>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- ADD FOOD SECTION -->
+      <div class="uppercase font-bold p-3">
+        <h2>Add food</h2>
+      </div>
+    </div>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
+#parent-section {
+  border-radius: 20px;
+  border: 1px solid #1a1a1a;
+  background-color: #1a1a1a;
+}
+
+.track-nutrition{
+  border-radius: 16px;
+  min-height: 260px;
+  border: 1px solid rgba(72,72,72,0.89);
+  background-color: rgba(72,72,72,0.89);
+}
+
+/*
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -27,4 +84,5 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+*/
 </style>
